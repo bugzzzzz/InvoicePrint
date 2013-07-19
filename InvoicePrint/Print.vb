eed657f839ac3b824185aabbd245232446bb4611
bugzzzzz <cindycheung@msn.com>
2013-07-19 16:45:57 +0800

    test

--- InvoicePrint/Print.vb
+++ InvoicePrint/Print.vb
@@ -213,4 +213,8 @@ Public Class Print
 
         Me.ReportViewer1.RefreshReport()
     End Sub
+
+    Private Sub ReportViewer1_Load(sender As System.Object, e As System.EventArgs) Handles ReportViewer1.Load
+
+    End Sub
 End Class
